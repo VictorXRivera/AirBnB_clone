6bd746d0bd536ac423b6d91b6b3bac01d5dc93e5
Mfeliciano778 <1652@holbertonschool.com>
2020-06-26 18:42:48 +0000

    Added instance method init and str

--- models/base_model.py
+++ models/base_model.py
@@ -1,4 +1,19 @@
 #!/usr/bin/python3
-""" Basemodel class """
-class BaseModel:
+'''Basemodel class'''
+from datetime import datetime
 
+
+class BaseModel:
+    '''BaseModel: defines all commmon attributes/methods for other classes
+    '''
+    def __init__(self, id):
+        '''__init__: instantiation of id, created_at, and updated_at
+        '''
+        self.id = uuid4(str(id))
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+
+    def __str__(self):
+        '''__str__: returns the string representation
+        '''
+        return "[BaseModel] (%d) <%s>".format(self.id, self.__dict__)
