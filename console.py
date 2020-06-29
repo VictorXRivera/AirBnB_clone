e884049b98afa1db9ceec0cb5665c3a862572706
VictorXRivera <victorxrivera.professional@gmail.com>
2020-06-29 15:59:39 +0000

    Added comments

--- console.py
+++ console.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
+""" Custom hbnb console """
 import cmd, sys
 
 class HBNBCommand(cmd.Cmd):
+    """ Class HBNBCommand """
     prompt = '(hbnb) '
 
     def do_quit(self, inp):
+        """ Quit command"""
         return True
 
     def help_quit(self):
+        """ Description of cmd """
         print('Quit command to exit the program')
     
     def default(self, inp):
+        """ Quit function """
         if inp == 'quit':
             return self.do_quit(inp)
 
