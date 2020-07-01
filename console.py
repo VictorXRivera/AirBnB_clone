f0cb202c6638f37dabac807a369fd4f502a41720
VictorXRivera <victorxrivera.professional@gmail.com>
2020-07-01 23:57:07 +0000

    Added help statements

--- console.py
+++ console.py
@@ -21,6 +21,26 @@ class HBNBCommand(cmd.Cmd):
     def help_quit(self):
         """ Description of cmd """
         print('Quit command to exit the program\n')
+
+    def help_create(self):
+        """ Description on create """
+        print('Creates a new instance of BaseModel, saves it (to the JSON file) and prints the id\n')
+
+    def help_show(self):
+        """ Desc. on show """
+        print('Prints the string representation of an instance based on the class name and id\n')
+
+    def help_destroy(self):
+        """ Desc. on destroy """
+        print('Deletes an instance based on the class name and id (save the change into the JSON file\n')
+
+    def help_all(self):
+        """ Desc. on all """
+        print('Prints all string representation of all instances based or not on the class name.\n')
+
+    def help_update(self):
+        """ Desc. on update """
+        print('Updates an instance based on the class name and id by adding or updating attribute (save the change into the JSON file)\n')
     
     def default(self, inp):
         """ Quit function """
