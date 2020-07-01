da8b3a7b590215bfd5c99797ec2a14a2f6be4559
Mfeliciano778 <1652@holbertonschool.com>
2020-07-01 20:51:49 +0000

    Fixed output error

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,6 @@
 '''Basemodel class'''
 from datetime import datetime as dt
 from uuid import uuid4
-from models.__init__ import storage
 
 
 class BaseModel:
@@ -11,17 +10,21 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         '''__init__: instantiation of id, created_at, and updated_at
         '''
+        from models.__init__ import storage
+
         if kwargs:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
                     setattr(self, key, dt.strptime(value,
                             "%Y-%m-%dT%H:%M:%S.%f"))
+                elif key == '__class__':
+                    setattr(self, key, type(self))
                 else:
                     setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = dt.now()
-            self.updated_at = dt.now()
+            self.updated_at = self.created_at
             storage.new(self)
 
     def __str__(self):
@@ -33,6 +36,8 @@ class BaseModel:
     def save(self):
         '''save: updates the public instance attribute updated_at
         '''
+        from models.__init__ import storage
+
         self.updated_at = dt.now()
         storage.save()
 
