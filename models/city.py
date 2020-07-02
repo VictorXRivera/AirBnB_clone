bfa30465d630ddc99053326a73f83249bcd5bd59
Mfeliciano778 <60366104+Mfeliciano778@users.noreply.github.com>
2020-07-01 22:35:49 -0400

    Tasks for 8 and 9 (#11)

---
+++ models/city.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''city: inherits from BaseModel'''
+
+
+class City(BaseModel):
+    '''City: stores City name (empty)
+    '''
+    state_id = ''
+    name = ''
---
+++ models/review.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''review: inherits from BaseModel'''
+
+
+class Review (BaseModel):
+    '''Review: stores reviews on places (empty)
+    '''
+    place_id = ''
+    user_id = ''
+    text = ''
