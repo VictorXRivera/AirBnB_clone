154246bafd33036dccf1d0834492ed25894816aa
VictorXRivera <victorxrivera.professional@gmail.com>
2020-07-11 04:30:52 +0000

    BaseModel class definition

--- models/base_model.py
+++ models/base_model.py
@@ -1,43 +1,46 @@
 #!/usr/bin/python3
-'''Basemodel class'''
+""" Basemodel class """
 from datetime import datetime as dt
-from uuid import uuid4
+import uuid
+import json
 
 
 class BaseModel:
-    '''BaseModel: defines all commmon attributes/methods for other classes
-    '''
+    """BaseModel: defines all commmon attributes/methods for other classes
+    """
     def __init__(self, *args, **kwargs):
-        '''__init__: instantiation of id, created_at, and updated_at
-        '''
-        from models.__init__ import storage
-
-        if kwargs:
+        """__init__: instantiation of id, created_at, and updated_at
+        """
+        if len(kwargs) != 0:
+            """ Use the dictionary to create an instance if given to program
+            """
             for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    setattr(self, key, dt.strptime(value,
-                            "%Y-%m-%dT%H:%M:%S.%f"))
-                elif key == '__class__':
+                if key == '__class__':
                     setattr(self, key, type(self))
+                elif key == 'created_at' or key == 'updated_at':
+                    setattr(self, key, dt.strptime(value,
+                                                   "%Y-%m-%dT%H:%M:%S.%f"))
                 else:
                     setattr(self, key, value)
         else:
-            self.id = str(uuid4())
+            """ Case of if no dictionary is given,
+            create a new instance with randomly generated id and current time """
+            self.id = str(uuid.uuid4())
             self.created_at = dt.now()
             self.updated_at = self.created_at
+            from models.__init__ import storage
             storage.new(self)
 
     def __str__(self):
         '''__str__: returns the string representation
         '''
-        return "[{}] ({}) {}".format(type(self).__name__,
-                                     self.id, self.__dict__)
+        return ("[{}] ({}) {}".format(type(self).__name__,
+                                     self.id, self.__dict__))
 
     def save(self):
         '''save: updates the public instance attribute updated_at
         '''
         from models.__init__ import storage
-
         self.updated_at = dt.now()
         storage.save()
 
@@ -51,5 +54,5 @@ class BaseModel:
                 new_dict[key] = value.isoformat()
             else:
                 new_dict[key] = value
-        new_dict["__class__"] = type(self).__name__
+        new_dict['__class__'] = type(self).__name__
         return new_dict
